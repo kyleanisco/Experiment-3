1d61741406bc12081ca2e53ff5ac2358d0c04ca3
kyleanisco <47727580+kyleanisco@users.noreply.github.com>
2019-05-10 12:04:28 +0800

    Add files via upload

---
+++ Prob1Exp3.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <conio.h>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	int Arr[10],n,i,small,large, sum=0;
+	float average;
+
+
+	n=10;
+
+	for(i=0;i<n;i++)
+	{
+		cout<<"Enter element "<<i+1<<":";
+		cin>>Arr[i];
+	}
+
+	small=Arr[0];
+	large=Arr[0];
+
+	for(i=1;i<n;i++)
+	{
+		if(Arr[i]<small)
+			small=Arr[i];
+		if(Arr[i]>large)
+			large=Arr[i];
+	}
+	
+		for(i=0;i<n;i++)
+	{
+		sum += Arr[i];
+	}
+	
+	average = (float)sum /n;
+
+	cout<<"\nLargest integer is :"<<large;
+	cout<<"\nSmallest integer is :"<<small;
+    
+    cout << "\nSum: " << sum<< endl;
+    cout << "Average: " << average;
+
+    getch();
+	return 0;
+}
+
